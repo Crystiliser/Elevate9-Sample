22a19823f1fa1003c85e17bcf18e47fe123c4687
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-01-16 15:59:32 -0600

    Update Layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Icon from './assets/logo-new-removebg-preview.png';
+import HeaderLogo from './assets/logo-new-removebg-preview.png';
+import FooterLogo from './assets/logo-embossed.png';
 
 
 const navItems = [
@@ -25,18 +27,28 @@ const navItems = [
     }
 ];
 
+const name = 'Elevate9.io';
+const phone = '555.555.555';
+const email = 'info@elevate.io';
+
 
 const Layout: React.FC = () => {
     return (
         <>
             <Header
-                icon={(<img src={Icon} />)}
+                icon={(<img src={HeaderLogo} height={121} width={147} alt="Elevate9 Logo" />)}
                 navItems={navItems}
             />
             <main>
                 <Outlet />
             </main>
-            <Footer />
+            <Footer
+              icon={(<img src={FooterLogo} alt="Elevate 9 Logo" />)}
+              name={name}
+              phone={phone}
+              email={email}
+              navItems={navItems}
+            />
         </>
     );
 };
