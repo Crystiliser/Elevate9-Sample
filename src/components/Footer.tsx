071d731ba26232b1fa3863e8602b7f00c612b9be
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-01-16 15:59:26 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,55 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
-
-import Logo from '../assets/logo-embossed.png';
 import { Typography } from '@mui/material';
 
 
-function Footer() {
+interface FooterProps {
+    icon: React.ReactNode,
+    name?: string,
+    phone?: string,
+    email?: string,
+    navItems: {
+        name: string,
+        link: string
+    }[]
+}
+
+
+function Footer(props: FooterProps) {
+
+    const {
+        icon,
+        name,
+        phone,
+        email,
+        navItems
+    } = props;
+
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid size={3}>
-                    <img src={Logo} alt="Elevate 9 Logo" />
-                </Grid>
-                <Grid size={3}>
-                    <Typography>
-                        Elevate9.io
-                    </Typography>
-                    <Typography>
-                        Phone: 555.555.555
-                    </Typography>
-                    <Typography>
-                        Email: info@elevate.io
-                    </Typography>
-                </Grid>
-                <Grid size={6}>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Home
-                    </Typography>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Services
-                    </Typography>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Get A Quote
-                    </Typography>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Connect
-                    </Typography>
-                </Grid>
+        <Grid container sx={{ width: '100%' }}>
+            <Grid size={3}>
+                {icon}
             </Grid>
-        </Box>
+            <Grid size={3}>
+                <Typography>
+                    {name}
+                </Typography>
+                <Typography>
+                    Phone: {phone}
+                </Typography>
+                <Typography>
+                    Email: {email}
+                </Typography>
+            </Grid>
+            <Grid size={6}>
+                {navItems.map((item) => (
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                        {item.name}
+                    </Typography>
+                ))}
+            </Grid>
+        </Grid>
     );
 }
 
