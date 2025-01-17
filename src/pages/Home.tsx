653cc1be05de3c33f482396907584e35f09f094b
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-01-17 14:13:05 -0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,18 @@ import { Box } from '@mui/material';
 import HeaderImage from '../assets/header.png';
 
 
+const ContainerStyle = {
+  backgroundImage: `url(${HeaderImage})`,
+  height: '82vh',
+  backgroundSize: '100%',
+  backgroundRepeat: 'no-repeat'
+};
+
+
 function Home() {
 
   return (
-    <Box sx={{ backgroundImage: `url(${HeaderImage})`, height: '82vh' }}>
+    <Box sx={ContainerStyle}>
       Coming Soon
     </Box>
   );
